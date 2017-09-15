f0f8e8a400f298f78a1e06f21ecfa1cb08cebb77
SanjunLiu <sanjun_liu@sina.com>
2017-09-15 16:08:19 +0800

    update train sh

--- multilabel/train_alexnet.sh
+++ multilabel/train_alexnet.sh
@@ -3,7 +3,7 @@ set -e
 
 CAFFE=./caffe
 SOLVER=multilabel/solver.prototxt
-#WEIGHTS=alexnet/bvlc_alexnet.caffemodel
+WEIGHTS=models/alexnet/alexnet_train_iter_5000.caffemodel
 
-#$CAFFE train --solver=$SOLVER --weights=$WEIGHTS --gpu 0 $@
-$CAFFE train --solver=$SOLVER --gpu 0 $@
+$CAFFE train --solver=$SOLVER --weights=$WEIGHTS --gpu 0 $@
+#$CAFFE train --solver=$SOLVER --gpu 0 $@
