287a30a1165ab7e0beb0b888409db271870e1ad6
SanjunLiu <sanjun_liu@sina.com>
2017-10-26 11:36:55 +0800

    add train resnet shell

---
+++ ResNet/train_resnet.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -e
+
+CAFFE=./caffe
+
+# resnet50
+SOLVER=ResNet/ResNet_50_solver.prototxt
+
+WEIGHTS=./models/resnet/resnet50_train_iter_40000.caffemodel
+SNAPSHOT=./models/resnet/resnet50_train_iter_40000.solverstate
+
+$CAFFE train --solver=$SOLVER --gpu 0,1,2,3 $@
+#$CAFFE train --solver=$SOLVER --weights=$WEIGHTS --gpu 0,1,2,3 $@
+#$CAFFE train --solver=$SOLVER --snapshot=$SNAPSHOT --gpu 2,3 $@
